eaf58f5c8105b880381f956281fe29fca2d6e521
Jay Rajendra Gite <jaygite544@gmail.com>
2024-07-07 17:19:09 +0530

    commit changes

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import '@babel/polyfill'
+import { useState } from 'react'
+import './App.css'
+import SpeechRecognition ,{useSpeechRecognition} from 'react-speech-recognition'   
+
+function App() {
+
+  const StartListenning = () => SpeechRecognition.startListening({ continuous: true })
+
+  const {transcript,browserSupportsSpeechRecognition } = useSpeechRecognition({})
+
+    if(!browserSupportsSpeechRecognition){
+      return null;
+    }
+
+
+  return (
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter </h2>
+        <br />
+      
+
+        <div className="main-contain">
+            {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button className='button'>Copy</button>
+          <button  onClick={StartListenning} className='button'>Start Listening </button>
+          <button onClick={() => SpeechRecognition.stopListening()}  className='button'>Stop Listening </button>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
